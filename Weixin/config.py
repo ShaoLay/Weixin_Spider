5f8c16f5fc6468aba3547f9ea705cd48226731aa
ShaoLeei <javs_shao@163.com>
2019-10-03 18:40:29 +0800

    配置文件

---
+++ Weixin/config.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: Hamdi
+# Redis配置
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_PASSWORD = '970202'
+REDIS_KEY = 'weixin'
+
+# 代理ip获取地址
+REDIS_POOL_URL = 'http://127.0.0.1:5555/random'
+
+# MySQL配置
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = 3306
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = '970202'
+MYSQL_DATABASE = 'weixin'
+
+# 超时时间
+TIMEOUT = 10
+MAX_FAILED_TIME = 20
+
+# 状态码
+VALID_STATUSES = [200]
+
+
