08fac25134a1b6d1e327d1f6d737d7bf389be566
ShaoLeei <javs_shao@163.com>
2019-10-03 18:43:46 +0800

    构造请求_WeixinRequest数据结构

--- Weixin/request.py
+++ Weixin/request.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # author: Hamdi
+from config import *
+from requests import Request
+
+
+class WeixinReuqest(Request):
+    def __init__(self, url, callback, method='GET', headers=None, need_proxy=False, fail_time=0, timeout=TIMEOUT):
+        Request.__init__(self, method, url, headers)
+        self.callback = callback
+        self.need_proxy = need_proxy
+        self.fail_time = fail_time
+        self.timeout = timeout
