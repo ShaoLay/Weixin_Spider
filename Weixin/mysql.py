2bb9000bb82c621dcc1f9c9d4f81d5bc16cc5391
ShaoLeei <javs_shao@163.com>
2019-10-04 10:46:28 +0800

    数据持久化_MySQL

--- Weixin/mysql.py
+++ Weixin/mysql.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # author: Hamdi
+from config import *
+import pymysql
+
+
+class MySQL():
+    def __init__(self, host=MYSQL_HOST, username=MYSQL_USER, password=MYSQL_PASSWORD,
+                 database=MYSQL_DATABASE):
+        """
+        MySQL初始化
+        :param host:
+        :param username:
+        :param password:
+        :param database:
+        """
+        try:
+            self.db = pymysql.connect(host, username, password, database, charset='utf8')
+            self.cursor = self.db.cursor()
+        except pymysql.MySQLError as e:
+            print(e.args)
+
+    def insert(self, table, data):
+        """
+        插入数据
+        :param table:
+        :param data:
+        :return:
+        """
+        keys = ', '.join(data.keys())
+        values = ', '.join(['%s'] * len(data))
+        sql_query = 'insert into %s (%s) values (%s)' % (table, keys, values)
+        try:
+            self.cursor.execute(sql_query, tuple(data.values()))
+            self.db.commit()
+        except pymysql.MySQLError as e:
+            print(e.args)
